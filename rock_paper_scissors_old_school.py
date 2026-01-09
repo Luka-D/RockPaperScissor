54b10e6c60f4777511aeecb0000f0225fe8f390a
Luka Dojcinovic <56648891+Luka-D@users.noreply.github.com>
2026-01-09 13:38:50 -0500

    fix: Reformatted V1 and renamed it to old school

--- rock_paper_scissors_old_school.py
+++ rock_paper_scissors_old_school.py
@@ -1,24 +1,8 @@
 import random, time, os
 
-from ASCII_sprites import ASCII_sprites
-
 
 def clear_screen():
     os.system("cls" if os.name == "nt" else "clear")
-
-
-def print_result(images):
-    strings_by_column = [s.split("\n") for s in images]
-    strings_by_line = zip(*strings_by_column)
-    max_length_by_column = [
-        max([len(s) for s in col_strings]) for col_strings in strings_by_column
-    ]
-    for parts in strings_by_line:
-        # Pad strings in each column so they are the same length
-        padded_strings = [
-            parts[i].ljust(max_length_by_column[i]) for i in range(len(parts))
-        ]
-        print("".join(padded_strings))
 
 
 def player_chooses():
@@ -45,54 +29,37 @@ def opponent_chooses():
     return opponents_choice
 
 
-def ro_sham_bo(num):
-    print(num)
-    print_result(ASCII_sprites["rock_rotated"])
-    time.sleep(0.3)
-    clear_screen()
-    print_result(ASCII_sprites["rock_rock"])
-    time.sleep(0.3)
-    clear_screen()
-
-
 def game(players_choice, opponents_choice):
-    clear_screen()
-    count = 3
-    while count > 0:
-        ro_sham_bo(count)
-        count -= 1
+    print("3")
+    time.sleep(0.5)
+    print("2")
+    time.sleep(0.5)
+    print("1")
+    time.sleep(0.5)
     print("Go!")
-    print_result(ASCII_sprites["rock_rotated"])
-    time.sleep(0.3)
-    clear_screen()
+    time.sleep(0.5)
+    print(f"Opponent chooses {opponents_choice}.")
+    time.sleep(0.5)
+    print(f"You chose {players_choice}.")
     if players_choice == "rock" and opponents_choice == "scissors":
-        print_result(ASCII_sprites["rock_scissors"])
         print("rock beats scissors. You win!")
     elif players_choice == "rock" and opponents_choice == "paper":
-        print_result(ASCII_sprites["rock_paper"])
         print("paper beats rock. You lose.")
     elif players_choice == "paper" and opponents_choice == "rock":
-        print_result(ASCII_sprites["paper_rock"])
         print("paper beats rock. You win!")
     elif players_choice == "paper" and opponents_choice == "scissors":
-        print_result(ASCII_sprites["paper_scissors"])
         print("scissors beat paper. You lose.")
     elif players_choice == "scissors" and opponents_choice == "paper":
-        print_result(ASCII_sprites["scissors_paper"])
         print("scissors beat paper. You win!")
     elif players_choice == "scissors" and opponents_choice == "rock":
-        print_result(ASCII_sprites["scissors_rock"])
         print("rock beats scissors. You lose.")
     elif players_choice == "rock" and opponents_choice == "rock":
-        print_result(ASCII_sprites["rock_rock"])
         print("Same result. Try again.")
     elif players_choice == "paper" and opponents_choice == "paper":
-        print_result(ASCII_sprites["paper_paper"])
         print("Same result. Try again.")
     elif players_choice == "scissors" and opponents_choice == "scissors":
-        print_result(ASCII_sprites["scissors_scissors"])
         print("Same result. Try again.")
-    play = input("Play again? (y = Yes, n = No) ").casefold()
+    play = input("Play again? (y = Yes, n = No) ")
     if play == "y":
         main()
     else:
